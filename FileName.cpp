513c11f52b03ceb20a3676415eb352935eaaec94
Diego Benitez <criptograma12@gmail.com>
2024-07-19 19:16:56 -0300

    agregamos sonido, background y movimientos a los Sprites

--- FileName.cpp
+++ FileName.cpp
@@ -1,25 +1,36 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 using namespace std;
 
 
 int main()
 {
+ 
     sf::Texture ballT;
     sf::Texture player1T;
     sf::Texture player2T;
+    sf::Texture backgroundT;
 
      
     sf::Sprite ballS;
     sf::Sprite player1S;
     sf::Sprite player2S;
+    sf::Sprite backgroundS;
     
 
+    sf::SoundBuffer dibuSoundB;
+    sf::Sound dibuSound;
 
-    float velX = 2;
-    float velY = 2;
+    bool flag = false;
+    float velX = 5;
+    float velY = 5;
 
+
+    if (!backgroundT.loadFromFile("campo.png")) {
+        cout << "Error al cargar textura campo" << endl;
+    }
     if (!ballT.loadFromFile("ball.png")) {
         cout << "Error al cargar textura ball" << endl;
     }
@@ -30,6 +41,16 @@ int main()
     if (!player2T.loadFromFile("kolo.png")) {
         cout << "Error al cargar textura Kolo" << endl;
     }
+    if (!dibuSoundB.loadFromFile("tecomo.ogg")) {
+        cout << "Error al cargar el sonido tecomo.ogg" <<endl;
+    }
+
+    //background texture
+    backgroundS.setTexture(backgroundT);
+    backgroundS.setOrigin(backgroundT.getSize().x / 2, backgroundT.getSize().y / 2);
+    backgroundS.setPosition(425, 250);
+    backgroundS.setScale(5, 5);
+    backgroundS.setScale(1, 1);
 
     //ball texture
     ballS.setTexture(ballT);
@@ -37,11 +58,12 @@ int main()
     ballS.setPosition(425, 250);
     ballS.setScale(0.02, 0.02);
 
-    //dibu texture
+    //dibu texture & sound
     player1S.setTexture(player1T);
     player1S.setOrigin(player1T.getSize().x / 2, player1T.getSize().y / 2);
     player1S.setPosition(805, 250);
     player1S.setScale(0.2, 0.2);
+    dibuSound.setBuffer(dibuSoundB);
 
     //kolo texture
     player2S.setTexture(player2T);
@@ -49,8 +71,10 @@ int main()
     player2S.setPosition(30, 250);
     player2S.setScale(0.2, 0.2);
 
+    
+
     sf::RenderWindow window(sf::VideoMode(850, 500), "Kolo Muani vs Dibu!");
-    window.setFramerateLimit(120);
+    window.setFramerateLimit(60);
     
     
  
@@ -72,8 +96,39 @@ int main()
         if (ballS.getPosition().y > 500 || ballS.getPosition().y < 0) {
             velY *= -1;
         }
+
+        //player1 movement
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            player1S.move(0, -10);
+            
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            player1S.move(0, 10);
+        }
+        //player2 movement
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            player2S.move(0, -10);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            player2S.move(0, 10);
+        }
+
+        //sound player1
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::K)&& !flag) {
+            flag = true;
+            dibuSound.play();
+            
+        }
+        else if (!sf::Keyboard::isKeyPressed(sf::Keyboard::K)) {
+            flag = false;
+        }
        
         window.clear();
+        window.draw(backgroundS);
         window.draw(ballS);
         window.draw(player1S);
         window.draw(player2S);
